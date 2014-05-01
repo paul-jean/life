342f3c93d10a5fd0094449f8b1d192be42c94f7e
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-05-01 00:32:33 -0400

    Reorganize implementations. Start adding clojurescript implementation.

---
+++ implementations/cljs-div/cljs_div.js
@@ -0,0 +1,4 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
+goog.addDependency("../clojure/string.js", ['clojure.string'], ['cljs.core', 'goog.string', 'goog.string.StringBuffer']);
+goog.addDependency("../cljs_div/core.js", ['cljs_div.core'], ['cljs.core', 'clojure.string']);
